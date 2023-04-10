907d6fd8f02b5103e2beeb569c60f5bbe95ef573
Fatimah Ismael <fatimahismael@yahoo.com>
2023-04-10 10:22:15 -0700

    get id passing

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,6 @@
+const Account = require('./accounts-model')
+
+
 exports.checkAccountPayload = (req, res, next) => {
   console.log('checkAccountPayload middleware')
   next()
@@ -10,6 +13,17 @@ exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    const account = await Account.getById(req.params.id)
+      if(!account){
+        next({status: 404, message: 'account not found'})
+      } else{
+        req.account = account
+        next()
+      }
+  }catch(err){
+    next(err)
+  }
 }
