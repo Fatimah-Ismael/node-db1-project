8d8b59335e1f0caf5e7f49dff1670160955d9ef5
Fatimah Ismael <fatimahismael@yahoo.com>
2023-04-10 09:58:27 -0700

    get accounts passing

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,6 @@
 exports.checkAccountPayload = (req, res, next) => {
+  console.log('checkAccountPayload middleware')
+  next()
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
